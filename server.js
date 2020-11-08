dedbd72213b5e5a3cbb6f79238771b782db495dc
slowdive <schlickjoe@gmail.com>
2020-11-08 01:17:03 -0800

    fixed seeder, added the rest of the routes to server, removed unused ones

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path")
 
 const PORT = process.env.PORT || 3000;
 
@@ -17,7 +18,53 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
+app.get("/exercise", (req,res) => {
+  res.sendFile(path.join(__dirname, "./public/exercise.html"))
+});
 
+app.get("/api/workouts", (req,res) => {
+  db.Exercise.find({})
+  .then(dbExercise => {
+    res.json(dbExercise);
+  })
+  .catch(err => {
+    res.json(err);
+  });
+});
+
+app.post("/api/workouts", (req,res) => {
+  db.Exercise.create({})
+  .then(dbExercise => {
+    res.json(dbExercise);
+  })
+  .catch(err => {
+    res.json(err);
+  });
+});
+
+app.put("/api/workouts/:id", ({ body, params }, res) => {
+  db.Exercise.findByIdAndUpdate(params.id, { $push: { exercises: body } },{new:true})
+  .then(dbExercise => {
+    res.json(dbExercise);
+  })
+  .catch(err => {
+    res.json(err);
+  });
+});
+
+app.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/stats.html"))
+});
+
+app.get("/api/workouts/range", (req, res) => {
+  db.Exercise.find({})
+    .then(dbExercise => {
+      res.json(dbExercise);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
